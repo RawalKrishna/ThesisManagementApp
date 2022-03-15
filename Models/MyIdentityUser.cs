3922eefd4f2b65c48fbc2261e3dd43a83e473c72
krishnaRawal95 <krishna.rawal@einfochips.com>
2022-03-15 22:09:57 +0530

    IdentityUser Update

---
+++ Models/MyIdentityUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GuniThesis.Models
+{
+    public class MyIdentityUser : IdentityUser<Guid>
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "{0} is Required")]
+        [MinLength(2, ErrorMessage = "{0} requires minimum {1} characters")]
+        [StringLength(50, ErrorMessage = "{0} cannot have more than {1} characters")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Date Of Birth")]
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        [PersonalData]
+        public MyIdentityGenders Gender { get; set; }  // enum to be created
+
+        //role type :
+
+        [Display(Name = "Is Admin User")]
+        [Required]
+        [MinLength(3)]
+        [StringLength(60)]
+        public string IsAdminUser { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using GuniThesis.Data;
+using GuniThesis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,7 +32,7 @@ namespace GuniThesis
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<MyIdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
